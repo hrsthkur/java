50418a96d62bd4c229d3dd03bef76f33c2e0ae16
Harsh Singh <hrsthkur1@gmail.com>
2026-03-16 22:40:54 +0530

    first object

---
+++ section_five(oop)/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+
+    public static void main(String[] args){
+
+        Car toyota = new Car();
+        toyota.make = "Toyota";
+        toyota.price = 100000;
+        toyota.year = 2020;
+        toyota.color = "Green";
+
+        System.out.println(toyota.make +" - " + toyota.price);
+
+    }
+}
