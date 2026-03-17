bfb6ecb1b6b9df29be995b034d69058f4a9f31e5
Harsh Singh <hrsthkur1@gmail.com>
2026-03-17 10:13:38 +0530

    Encapsulation: setter functions

--- section_five(oop)/Car.java
+++ section_five(oop)/Car.java
@@ -27,5 +27,21 @@ public class Car {
         return color;
     }
 
+    public void setMake(String make){
+        this.make = make;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public void setYear(int year){
+        this.year = year;
+    }
+
+    public void setColor(String color){
+        this.color = color;
+    }
+
 
 }
--- section_five(oop)/Main.java
+++ section_five(oop)/Main.java
@@ -4,8 +4,8 @@ public class Main {
     public static void main(String[] args){
 
         Car toyota = new Car("Toyota",100000,2020,"Green");
-
-        System.out.println(toyota.getMake() +" - " + toyota.getPrice());
+        toyota.setColor("New color - black");
+        System.out.println(toyota.getMake() +" - " + toyota.getPrice() +" - " + toyota.getColor());
 
     }
 }
